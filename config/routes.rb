11e1bdb5f69e89817ea5e0956be8f8bd66d0002a
Rafael Quintanilha <rafael.quintanilha18@gmail.com>
2022-08-19 11:51:49 +0100

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'lists#index'
   resources :movies do
     resources :bookmarks
   end
 
-  resources :lists do
+  resources :lists, except: :index do
     resources :bookmarks, only: [:new,:create]
   end
   resources :bookmarks, only: :destroy
